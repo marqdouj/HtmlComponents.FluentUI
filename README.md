443f2c62531ea7c3c164453e098496c98cbf51c4
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-06-07 05:01:25 -0600

    **8.0.0** - Added ColorPicker component - Added Theme components - Created Package

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # HtmlComponents.FluentUI
+- This is a collection of custom Html components based on FluentUI-Blazor.
+- How to setup and configure [FluentUI-Blazor](https://www.fluentui-blazor.net/). 
+
+## Demo
+- The local 'Sandbox' project contains a demo on using the components
+- A demo of this and some of my other NuGet packages can be found [here](https://github.com/marqdouj/BlazorSandbox)
+
+## NuGet Package
+- https://www.nuget.org/packages/Marqdouj.HtmlComponent.FluenUI/
+
+## Components
+- **ColorPicker**: Component for selecting an HTML Color Name
+  - uses the HtmlColorName enum in my [HTML Common](https://github.com/marqdouj/HTMLCommon) NuGet package.
+- [**Themes**](https://www.fluentui-blazor.net/DesignTheme)
+  - **ThemeSwitch**: Component for toggling between light and dark themes.
+  - **ThemeMode**: Component for selecting between system, light, and dark themes.
+  - **ThemeColor**: Component for selecting theme color.
+
+## Release Notes
+**8.0.0**
+- Added ColorPicker component
+- Added Theme components
+- Created Package
---
+++ src/Marqdouj.HtmlComponents.FluentUI/Marqdouj.HtmlComponents.FluentUI/ColorPickerOptions.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Marqdouj.HtmlComponents.FluentUI
+{
+    /// <summary>
+    /// <see Based on: cref="https://www.fluentui-blazor.net/Combobox"/>
+    /// </summary>
+    public class ColorPickerOptions
+    {
+        /// <summary>
+        /// <see cref="FluentSelect{TOption}.Appearance"/>
+        /// </summary>
+        public Appearance? Appearance { get; set; } = null;
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.AriaLabel"/>
+        /// </summary>
+        /// 
+        public string? AriaLabel { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Autofocus"/>
+        /// </summary>
+        public bool Autofocus { get; set; }
+
+        /// <summary>
+        /// <see cref="ListComponentBase{TOption}.ChangeOnEnterOnly"/>
+        /// </summary>
+        public bool ChangeOnEnterOnly { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentComponentBase.Class"/>
+        /// </summary>
+        public string? Class { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Disabled"/>
+        /// </summary>
+        public bool Disabled { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.DisplayName"/>
+        /// </summary>
+        public string? DisplayName { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Embedded"/>
+        /// </summary>
+        public bool Embedded { get; set; }
+
+        /// <summary>
+        /// <see cref="ListComponentBase{TOption}.Height"/>
+        /// </summary>
+        public string? Height { get; set; } = "200px";
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Immediate"/>
+        /// </summary>
+        public bool Immediate { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.ImmediateDelay"/>
+        /// </summary>
+        public int ImmediateDelay { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Label"/>
+        /// </summary>
+        public string? Label { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Name"/>
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentSelect{TOption}.Open"/>"/>
+        /// </summary>
+        public bool? Open { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Placeholder"/>
+        /// </summary>
+        public string? Placeholder { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentSelect{TOption}.Position"/>
+        /// </summary>
+        public SelectPosition Position { get; set; } = SelectPosition.Below;
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.ReadOnly"/>
+        /// </summary>
+        public bool ReadOnly { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentInputBase{TValue}.Required"/>
+        /// </summary>
+        public bool Required { get; set; }
+
+        /// <summary>
+        /// <see cref="FluentComponentBase.Style"/>"/>
+        /// </summary>
+        public string? Style { get; set; }
+
+        /// <summary>
+        /// <see cref="ListComponentBase{TOption}.Width"/>
+        /// </summary>
+        public string? Width { get; set; }
+    }
+}
